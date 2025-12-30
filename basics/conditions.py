b81cf69a938d8e5cf9f9899ec1f8d8f5cfba9b9f
sanket-koyande <koyandesanket20@gmail.com>
2025-12-30 13:14:02 +0530

    added conditional statements file

---
+++ basics/conditions.py
@@ -0,0 +1,20 @@
+# The if statement 
+x = 10 
+if x > 5: 
+    print("x is greater than 5") 
+ 
+# The if … else Statement 
+y = 4 
+if y > 5: 
+    print("y is greater than 5") 
+else: 
+    print("y is less than or equal to 5") 
+ 
+# The if … elif … else Statement 
+z = 8 
+if z > 10: 
+    print("z is greater than 10") 
+elif z == 8: 
+    print("z is equal to 8") 
+else: 
+    print("z is less than 10")
